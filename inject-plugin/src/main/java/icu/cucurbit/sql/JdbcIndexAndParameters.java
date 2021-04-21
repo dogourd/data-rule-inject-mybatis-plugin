1ccaf76cc83e1b2a76c6958f78a96a3a40adc76b
yuwenzhicheng <yuwzc@jianjukeji.com>
2021-04-21 09:18:12 +0800

    修复多规则参数设置错误问题

---
+++ inject-plugin/src/main/java/icu/cucurbit/sql/JdbcIndexAndParameters.java
@@ -0,0 +1,46 @@
+package icu.cucurbit.sql;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JdbcParameterAdder {
+
+    private int index;
+    private int nextIndex;
+    private Map<Integer, Object> mapping = new HashMap<>();
+
+    public void skipOne() {
+        index ++;
+    }
+
+    public void addParameter(Object value) {
+        if (index < nextIndex) {
+            index = nextIndex;
+        }
+        if (mapping.containsKey(index)) {
+            throw new IllegalArgumentException("index " + index + " already use");
+        }
+        mapping.put(index, value);
+        nextIndex = index + 1;
+    }
+
+    public void addParameters(Collection<Object> values) {
+        if (index < nextIndex) {
+            index = nextIndex;
+        }
+        if (mapping.containsKey(index)) {
+            throw new IllegalArgumentException("index " + index + " already use");
+        }
+        int curIndex = index;
+        for (Object value : values) {
+            mapping.put(curIndex ++, value);
+        }
+        nextIndex = curIndex;
+    }
+
+    public Map<Integer, Object> getMapping() {
+        return mapping;
+    }
+
+}
